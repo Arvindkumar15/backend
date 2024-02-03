d64b0b6912932f434efa2898f6ad78a7fbfb20c9
Arvind Kumar <arvindkumar31999@gmail.com>
2024-02-03 14:19:12 +0530

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, logginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { logOutUser, logginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
